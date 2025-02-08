5006f2d3a67254406238b0d42e3a0fcce3d0fc92
marder <marcin.derucki@gmail.com>
2025-02-08 16:35:39 +0100

    08.02.2025

---
+++ 09_functions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void addNumbers(int n1, int n2)
+{
+
+    int sum = n1 + n2;
+
+    printf("The sum is %d\n", sum);
+}
+
+int main()
+{
+
+    addNumbers(12, 8);
+
+    return 0;
+}
