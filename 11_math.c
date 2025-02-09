99074314573a2d6c453cf19c0c0ef8bcb15d2656
marder <marcin.derucki@gmail.com>
2025-02-09 11:17:27 +0100

    09.02.2025

---
+++ 11_math.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+    int num = 81;
+    int num2 = 27;
+
+    // Find the square root
+    double root = sqrt(num);
+    double root2 = sqrt(num);
+
+    printf("Square root of %d is %.2lf\n", num, root);
+    printf("Cube root of %d is %.2lf\n", num2, root2);
+
+    return 0;
+}
