754f903b6d7c5b6f0685bea36067a6c36335b08d
marder <marcin.derucki@gmail.com>
2025-02-09 11:51:15 +0100

    09.02.2025

--- 11_math.c
+++ 11_math.c
@@ -7,12 +7,17 @@ int main()
     int num = 81;
     int num2 = 27;
 
+    int base = 2;
+    int power = 3;
+
     // Find the square root
     double root = sqrt(num);
-    double root2 = sqrt(num);
+    double root2 = cbrt(num2);
+    int result = pow(base, power);
 
     printf("Square root of %d is %.2lf\n", num, root);
     printf("Cube root of %d is %.2lf\n", num2, root2);
+    printf("2^3=%d\n", result);
 
     return 0;
 }
